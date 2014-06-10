f7c13db9f17cf477592bcfbe802b0884d55b62a2
coreyshuman <ctshumancode@gmail.com>
2014-06-10 00:26:15 -0700

    Add support for distance and bearing for each post. List activity now implements positional data. Measured bearing (mag) seems off, may need calib.

--- src/com/mahshu/globalcomments/CommentListActivity.java
+++ src/com/mahshu/globalcomments/CommentListActivity.java
@@ -1,5 +1,6 @@
 package com.mahshu.globalcomments;
 
+import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -39,9 +40,14 @@ import android.app.Fragment;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v4.app.FragmentActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -51,6 +57,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -61,7 +68,8 @@ import android.widget.Toast;
 public class CommentListActivity extends FragmentActivity
 						implements GooglePlayServicesClient.ConnectionCallbacks,
 						GooglePlayServicesClient.OnConnectionFailedListener,
-						com.google.android.gms.location.LocationListener
+						com.google.android.gms.location.LocationListener,
+						SensorEventListener
 						{
 	
 	private static final long UPDATE_INTERVAL = 5000; //ms
@@ -93,11 +101,21 @@ public class CommentListActivity extends FragmentActivity
 	private boolean mapIsLoaded;
 	private boolean mapIsDoneInit;
 	private Marker curLocMarker = null;
+	private ListView postsView;
 	private Button btnGotoMap;
 	private EditText SearchRange;
+	private Button SearchUpdate;
 	private Location currentLocation;
 	private Location lastLocation;
 	private String selectedObjectId;
+	
+	private SensorManager mSensorManager;
+	Sensor accelerometer;
+	Sensor magnetometer;
+    float[] mGravity;
+    float[] mGeomagnetic;
+	Float azimuth;
+	private long lastUpdateTick;
 	
 	  // Adapter for the Parse query
 	  private ParseQueryAdapter<GlobalPost> posts;
@@ -110,10 +128,12 @@ public class CommentListActivity extends FragmentActivity
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
+	    getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 	    mapIsLoaded = false;
 	    mapIsDoneInit = false;
 	    radius = 5000;	//cts debug - hardcode
 	    lastRadius = radius;
+	    lastUpdateTick = 0;
 	    Log.d("GlobCom", "commentlist onCreate");
 	    setContentView(R.layout.activity_commentlist);
 	    lr = LocationRequest.create();
@@ -122,6 +142,10 @@ public class CommentListActivity extends FragmentActivity
 	    lr.setFastestInterval(1000*60); //cts debug
 	    lc = new LocationClient(this.getApplicationContext(), this, this);
 	    lc.connect();
+	    
+	    mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+	    accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+	    magnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 	    
 	    btnGotoMap = (Button)findViewById(R.id.cl_btnGotoMap);
 	    
@@ -132,6 +156,26 @@ public class CommentListActivity extends FragmentActivity
 				Intent nextIntent = new Intent(CommentListActivity.this, MapActivity.class);
 				startActivity(nextIntent);
 			}
+	    });
+	    
+	    SearchRange = (EditText) findViewById(R.id.cl_distText);
+	    SearchUpdate = (Button) findViewById(R.id.cl_updateDistBtn);
+	    SearchRange.setText(Float.toString(radius));
+	    
+	    SearchUpdate.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				radius = Float.parseFloat(SearchRange.getText().toString());
+	    		if(radius > 100000){
+	    			radius = 100000;
+	    			SearchRange.setText(Float.toString(radius));
+	    		}
+	    		
+	    		doListQuery();
+				
+			}
+	    	
 	    });
 
 	    // Set up a customized query
@@ -162,8 +206,16 @@ public class CommentListActivity extends FragmentActivity
 	        final TextView upvoteCount = (TextView) view.findViewById(R.id.lv_upvoteText);
 	        final ImageView downvoteView = (ImageView) view.findViewById(R.id.lv_downvoteImage);
 	        final TextView downvoteCount = (TextView) view.findViewById(R.id.lv_downvoteText);
+	        final TextView distanceView = (TextView) view.findViewById(R.id.lv_distanceText);
+	        final ImageView directionView = (ImageView) view.findViewById(R.id.lv_dirImage);
+	        
+	        
 
+	        //directionView.setRotation((float) calculateLatLngBearing(geoPointFromLocation(currentLocation) ,post.getLocation()));
+	        directionView.setTag(Float.valueOf((float)calculateLatLngBearing(geoPointFromLocation(currentLocation) ,post.getLocation())));
+	        distanceView.setText(getStringGeoPointDistance(geoPointFromLocation(currentLocation), post.getLocation()));
 
+	        
 	        
 	        contentView.setText(post.getText());
 	        usernameView.setText(post.getUser().getUsername());
@@ -222,7 +274,7 @@ public class CommentListActivity extends FragmentActivity
 	    posts.setPaginationEnabled(false);
 
 	    // Attach the query adapter to the view
-	    ListView postsView = (ListView) this.findViewById(R.id.commentListView);
+	    postsView = (ListView) this.findViewById(R.id.commentListView);
 	    postsView.setAdapter(posts);	    
 
 	}
@@ -269,15 +321,53 @@ public class CommentListActivity extends FragmentActivity
     	    LatLng myLatLng = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());      
     	  
         }
+        // enable accel and mag
+        mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
+        mSensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_UI);
+        
+        //update list
         doListQuery();
+    }
+    
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+    
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {  }
+    
+    public void onSensorChanged(SensorEvent event) {
+    	if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+    	      mGravity = event.values.clone();
+    	    if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+    	      mGeomagnetic = event.values.clone();
+    	    if (mGravity != null && mGeomagnetic != null) {
+    	      float Rf[] = new float[9];
+    	      float If[] = new float[9];
+    	      boolean success = SensorManager.getRotationMatrix(Rf, If, mGravity, mGeomagnetic);
+    	      if (success) {
+    	        float orientation[] = new float[3];
+    	        SensorManager.getOrientation(Rf, orientation);
+    	        azimuth = orientation[0]; // orientation contains: azimuth, pitch and roll
+
+    	        updateVisibleDirectionMarkers(azimuth);
+    	        
+    	      }
+    	    }
     }
 	
 	@Override
     public void onLocationChanged(Location loc) {
-		Log.d("GlobCom", "CL locationChanged");
 		currentLocation = loc;
+		if (lastLocation != null
+		        && geoPointFromLocation(loc)
+		            .distanceInKilometersTo(geoPointFromLocation(lastLocation)) < 0.01) {
+		      // If the location hasn't changed by more than 10 meters, ignore it.
+		      return;
+		    }
+		Log.d("GlobCom", "CL locationChanged");
 		lastLocation = loc;
-		LatLng pos = new LatLng(loc.getLatitude(), loc.getLongitude());
+		
 		doListQuery();
 
 	}
@@ -309,6 +399,82 @@ public class CommentListActivity extends FragmentActivity
         posts.loadObjects();
         Log.d("GlobCom", "CL updateListView");
       }
+    }
+    
+    /*
+     * Return STRING distance between two coordinates in miles or feet
+     */
+    private String getStringGeoPointDistance(ParseGeoPoint pnt1, ParseGeoPoint pnt2)
+    {
+    	NumberFormat formatter = NumberFormat.getNumberInstance();
+    	double dist=calculateGeoPointDistance(pnt1, pnt2)*0.621371;
+    	String distStr;
+    	if(dist < 0.189394)
+    	{
+    		dist = dist * 5280;
+    		formatter.setMaximumFractionDigits(0);
+    		distStr = formatter.format(dist) + "ft";
+    	}
+    	else
+    	{
+    		formatter.setMaximumFractionDigits(1);
+    		distStr = formatter.format(dist) + "mi";
+    	}
+    	return distStr;
+    }
+    
+    /*
+     * Return distance between two coordinates in km
+     */
+    private double calculateGeoPointDistance(ParseGeoPoint pnt1, ParseGeoPoint pnt2)
+    {
+    	double R = 6371; //km
+    	double lr1 = Math.toRadians(pnt1.getLatitude());
+    	double lr2 = Math.toRadians(pnt2.getLatitude());
+    	double diflat = Math.toRadians(pnt2.getLatitude() - pnt1.getLatitude());
+    	double diflon = Math.toRadians(pnt2.getLongitude() - pnt1.getLongitude());
+    	double a = Math.sin(diflat/2) * Math.sin(diflat/2) +
+    			Math.cos(lr1) * Math.cos(lr2) *
+    			Math.sin(diflon/2) * Math.sin(diflon/2);
+    	double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    	double dist = R * c;
+    	return dist;
+    }
+    
+    /*
+     * Return bearing between two coordinates in degrees
+     */
+    private double calculateLatLngBearing(ParseGeoPoint pnt1, ParseGeoPoint pnt2)
+    {
+    	double latr1 = Math.toRadians(pnt1.getLatitude());
+    	double latr2 = Math.toRadians(pnt2.getLatitude());
+    	double lonr1 = Math.toRadians(pnt1.getLongitude());
+    	double lonr2 = Math.toRadians(pnt2.getLongitude());
+    	double y = Math.sin(lonr2 - lonr1) * Math.cos(latr2);
+    	double x = Math.cos(latr1)*Math.sin(latr2) -
+    			Math.sin(latr1)*Math.cos(latr2)*Math.cos(lonr2 - lonr1);
+    	double bearing = Math.toDegrees(Math.atan2(y,x));
+    	return bearing;
+    }
+    
+    private void updateVisibleDirectionMarkers(float az)
+    {
+    	long tick = SystemClock.uptimeMillis();
+    	if(tick - lastUpdateTick < 1000)
+    		return;
+    	lastUpdateTick = tick;
+    	if(postsView.getChildCount() > 0) {
+        	int firstItem = postsView.getFirstVisiblePosition() - postsView.getHeaderViewsCount();
+        	int itemCnt = (postsView.getChildCount()-firstItem < 6) ? postsView.getChildCount()-firstItem : 6;
+        	int i;
+        	for(i=firstItem; i<firstItem+itemCnt; i++)
+        	{
+    	        View viewItem = (View)postsView.getChildAt(i);
+    	        ImageView directionView = (ImageView) viewItem.findViewById(R.id.lv_dirImage);
+    	        float offset = (Float)directionView.getTag();
+    	        directionView.setRotation(-az*360/(2*3.14159f) + offset);
+        	}
+        }
     }
     
 }
